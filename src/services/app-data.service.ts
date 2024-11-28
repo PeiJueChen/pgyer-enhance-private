3fc0bec294b30f361b8f27353f2b108e7cd5dae6
peijue.chen <peijue.chen@aigens.cn>
2024-11-28 23:10:40 +0800

    feat: enhance

--- src/services/app-data.service.ts
+++ src/services/app-data.service.ts
@@ -27,13 +27,17 @@ export class AppDataService {
     const password = this.currentAppInfo?.buildPassword;
     const buildKey = item.buildKey;
     if (!buildKey) return "";
-    if (this.platform === 'ios') {
-      const url = `itms-services://?action=download-manifest&url=https://www.pgyer.com/app/plist/${buildKey}`;
-      return !!password ? `${url}?password=${password}` : url;
-    } else {
-      const url = `https://www.pgyer.com/apiv2/app/install?_api_key=0202f5206763d902070f95c7826cb794&buildKey=${buildKey}`;
-      return !!password ? `${url}&buildPassword=${password}` : url;
-    }
+
+    const url = `https://www.pgyer.com/apiv2/app/install?_api_key=0202f5206763d902070f95c7826cb794&buildKey=${buildKey}`;
+    return !!password ? `${url}&buildPassword=${password}` : url;
+
+    // if (this.platform === 'ios') {
+    //   const url = `itms-services://?action=download-manifest&url=https://www.pgyer.com/app/plist/${buildKey}`;
+    //   return !!password ? `${url}?password=${password}` : url;
+    // } else {
+    //   const url = `https://www.pgyer.com/apiv2/app/install?_api_key=0202f5206763d902070f95c7826cb794&buildKey=${buildKey}`;
+    //   return !!password ? `${url}&buildPassword=${password}` : url;
+    // }
 
 
   }
